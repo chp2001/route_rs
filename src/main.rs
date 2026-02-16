09aa4e2e3596ee651a921308cc0f568b1d678d2c
chp2001 <chp.2001.jhp@gmail.com>
2026-02-16 16:27:56 -0600

    Split primary main functionality into `run_routing()` function, added tests for `get_simulation_params` and the new function that use the provided test case

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,10 @@ fn main() -> Result<()> {
     // Configuration
     //let (_, csv_dir, db_path, internal_timestep_seconds, output_dir)
     let config: cli::Config = get_args()?;
+    run_routing(config)
+}
+
+fn run_routing(config: cli::Config) -> Result<()> {
     let dt: f32 = config.internal_timestep_seconds as f32;
     let db_path: std::path::PathBuf = config.gpkg_file;
     let csv_dir: std::path::PathBuf = config.csv_dir;
@@ -167,4 +171,157 @@ fn get_simulation_params(
         .context("Failed to parse reference time")?;
 
     Ok((max_external_steps, reference_time))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::kernel::muskingum;
+
+    // Same-file tests for main file
+    use super::*;
+
+    // Can't easily test the full routing process without
+    // creating test data, but we can use the included ./tests/one_cat/ dataset
+    // to test for expected results
+
+    fn setup_test_config() -> cli::Config {
+        cli::Config {
+            config_dir: std::path::PathBuf::from("./tests/one_cat/config"),
+            csv_dir: std::path::PathBuf::from("./tests/one_cat/outputs/ngen"),
+            gpkg_file: std::path::PathBuf::from("./tests/one_cat/config/cat-486888_subset.gpkg"),
+            internal_timestep_seconds: 300,
+            output_dir: std::path::PathBuf::from("./tests/one_cat/outputs/troute"),
+            kernel: muskingum::MuskingumCungeKernel::TRouteModernized,
+        }
+    }
+
+    // Test get_simulation_params with the included test dataset
+
+    #[test]
+    fn test_get_simulation_params() {
+        // Test that get_simulation_params correctly reads the CSV file and extracts the max external steps and reference time
+        let config: cli::Config = setup_test_config();
+        let conn: rusqlite::Connection = rusqlite::Connection::open(&config.gpkg_file).unwrap();
+        let column_config: ColumnConfig = ColumnConfig::new();
+        let topology: network::NetworkTopology =
+            build_network_topology(&conn, &column_config, &config.csv_dir).unwrap();
+        let channel_params_map: HashMap<u32, ChannelParams> =
+            network::load_channel_parameters(&conn, &topology, &column_config).unwrap();
+
+        let (max_external_steps, reference_time) =
+            get_simulation_params(&config.csv_dir, &channel_params_map).unwrap();
+
+        assert_eq!(max_external_steps, 24);
+        assert_eq!(
+            reference_time,
+            NaiveDateTime::parse_from_str("2010-01-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()
+        );
+    }
+
+    #[test]
+    fn test_invalid_csv_file() {
+        // Test that get_simulation_params returns an error when the CSV file is missing or invalid
+        let config: cli::Config = setup_test_config();
+        let conn: rusqlite::Connection = rusqlite::Connection::open(&config.gpkg_file).unwrap();
+        let column_config: ColumnConfig = ColumnConfig::new();
+        let topology: network::NetworkTopology =
+            build_network_topology(&conn, &column_config, &config.csv_dir).unwrap();
+        let channel_params_map: HashMap<u32, ChannelParams> =
+            network::load_channel_parameters(&conn, &topology, &column_config).unwrap();
+
+        // Test with non-existent file
+        let invalid_csv_dir = std::path::PathBuf::from("./tests/one_cat/outputs/invalid_csv");
+        let result: std::result::Result<(usize, NaiveDateTime), anyhow::Error> =
+            get_simulation_params(&invalid_csv_dir, &channel_params_map);
+
+        assert!(result.is_err());
+        assert!(
+            result
+                .err()
+                .unwrap()
+                .to_string()
+                .contains("Failed to read file:")
+        );
+    }
+
+    #[test]
+    fn test_empty_features() {
+        // Test that get_simulation_params returns an error when the features map is empty
+        let empty_features: HashMap<u32, ChannelParams> = HashMap::new();
+        let result = get_simulation_params(
+            &std::path::PathBuf::from("./tests/one_cat/outputs/ngen"),
+            &empty_features,
+        );
+        assert!(result.is_err());
+        assert!(
+            result
+                .err()
+                .unwrap()
+                .to_string()
+                .contains("No features found")
+        );
+    }
+
+    #[test]
+    fn test_invalid_feature() {
+        // Test that get_simulation_params returns an error when the first feature ID does not correspond to a valid CSV file
+        let config: cli::Config = setup_test_config();
+        let mut invalid_features: HashMap<u32, ChannelParams> = HashMap::new();
+        invalid_features.insert(
+            999999,
+            ChannelParams {
+                dx: 0.0,
+                n: 0.0,
+                ncc: 0.0,
+                s0: 0.0,
+                bw: 0.0,
+                tw: 0.0,
+                twcc: 0.0,
+                cs: 0.0,
+            },
+        );
+        let result = get_simulation_params(&config.csv_dir, &invalid_features);
+        assert!(result.is_err());
+        assert!(
+            result
+                .err()
+                .unwrap()
+                .to_string()
+                .contains("Failed to read file:")
+        );
+    }
+
+    // Do basic test for run_routing with the included test dataset, just to check that it runs without error and produces output files
+    // Ideally we want to fail if any part of the routing process fails
+
+    #[test]
+    fn test_run_routing() {
+        let config: cli::Config = setup_test_config();
+        let result = run_routing(config);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_run_invalid_config() {
+        // Test that run_routing returns an error when given an invalid configuration (e.g. non-existent database file)
+        let invalid_config = cli::Config {
+            config_dir: std::path::PathBuf::from("./tests/invalid_test/config"),
+            csv_dir: std::path::PathBuf::from("./tests/invalid_test/outputs/ngen"),
+            gpkg_file: std::path::PathBuf::from(
+                "./tests/invalid_test/config/cat-486888_subset.gpkg",
+            ),
+            internal_timestep_seconds: 300,
+            output_dir: std::path::PathBuf::from("./tests/invalid_test/outputs/troute"),
+            kernel: muskingum::MuskingumCungeKernel::TRouteModernized,
+        };
+        let result = run_routing(invalid_config);
+        assert!(result.is_err());
+        assert!(
+            result
+                .err()
+                .unwrap()
+                .to_string()
+                .contains("Failed to open database:")
+        );
+    }
 }
